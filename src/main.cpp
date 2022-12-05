62f917bb7a445aa9657c8438762edf8d359f1044
Karmy <32240709+KarmyDev@users.noreply.github.com>
2022-12-05 18:39:36 +0100

    Add files via upload

---
+++ src/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+// for Windows compilation those args in main() are required
+int main(int argv, char** args)
+{
+    std::cout << "Hello, Karmy's build system!" << std::endl;
+    return 0;
+}
